3d673390dca7d247f2e8b5e3eda8ad4e396ff559
Federico Sapuppo <fedesapuppo@hotmail.com>
2025-03-20 18:39:44 -0300

    remove old template files and update webpack config

--- webpack.common.js
+++ webpack.common.js
@@ -1,32 +1,25 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     entry: './src/index.js',
     output: {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
-        clean: true,
+        clean: true
     },
-    plugins: [
-        new HtmlWebpackPlugin({
-            template: './src/index.html',
-        }),
-    ],
     module: {
         rules: [
             {
-                test: /\.html$/i,
-                loader: 'html-loader',
-            },
-            {
                 test: /\.css$/,
-                use: ['style-loader', 'css-loader'],
+                use: ['style-loader', 'css-loader']
             },
             {
                 test: /\.(png|svg|jpg|jpeg|gif)$/i,
-                type: 'asset/resource',
-            },
-        ],
+                type: 'asset/resource'
+            }
+        ]
     },
+    resolve: {
+        extensions: ['.js']
+    }
 };
